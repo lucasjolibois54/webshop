845994e351dd45bb0da17bb3f73711e9f3963962
Lucas Bendix Jolibois <lucasjolibois@icloud.com>
2023-03-02 22:28:06 +0100

    Update Home.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Nav from '../Components/Nav'
 import { useState, useEffect } from 'react';
+import { Link, Outlet } from 'react-router-dom'
 
 
 function Home() {
@@ -19,22 +20,22 @@ function Home() {
   return (
     <>
     <Nav/>
-    <div class="bg-white">
-        <div class="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-        <h2 class="sr-only">Products</h2>
+    <div className="bg-white">
+        <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+        <h2 className="sr-only">Products</h2>
 
-        <div class="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+        <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
             {products.map((product) => {
                 return (
                     <div className="product-card" key={product.title}>
 
-                    <a href="#" class="group">
-                        <div class="aspect-w-1 aspect-h-1 h-96 overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
-                            <img src={product.filename} alt={product.title} class="h-96 w-full object-cover object-center group-hover:opacity-75"/>
+                    <Link to="/product-page" className="group">
+                        <div className="aspect-w-1 aspect-h-1 h-96 overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
+                            <img src={product.filename} alt={product.title} className="h-96 w-full object-cover object-center group-hover:opacity-75"/>
                         </div>
-                    <h3 class="mt-4 text-sm text-gray-700">{product.title}</h3>
-                    <p class="mt-1 text-lg font-medium text-gray-900">DKK.{product.price}</p>
-                    </a>   
+                    <h3 className="mt-4 text-sm text-gray-700">{product.title}</h3>
+                    <p className="mt-1 text-lg font-medium text-gray-900">DKK.{product.price}</p>
+                    </Link>   
                     </div>
                 );
             })}
@@ -42,6 +43,7 @@ function Home() {
         </div>
         </div>
     </div>
+    <Outlet/>
     </>
   )
 }
