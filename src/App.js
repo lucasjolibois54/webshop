d2cc1ac45d32820578f89af241b8ba3f51a8dcac
Lucas Bendix Jolibois <lucasjolibois@icloud.com>
2023-03-02 19:52:04 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import {Routes, Route} from 'react-router-dom'
 import Home from './Pages/Home';
 import ProductPage from './Pages/ProductPage';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
