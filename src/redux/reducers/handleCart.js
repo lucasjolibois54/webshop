941f5c6a5cf185f64f59310f414072cbe813f41c
Lucas Bendix Jolibois <lucasjolibois@icloud.com>
2023-03-07 22:12:17 +0100

    Create handleCart.js

---
+++ src/redux/reducers/handleCart.js
@@ -0,0 +1,42 @@
+const cart = [];
+
+const handleCart = (state = cart, action) => {
+  const product = action.payload;
+  const exist = state.find((x) => x.id === product.id);
+
+  switch (action.type) {
+    case "ADDITEM":
+      if (exist) {
+        // Increase the quantity
+        return state.map((x) =>
+          x.id === product.id ? { ...x, qty: x.qty + 1 } : x
+        );
+      } else {
+        const product = action.payload;
+        return [
+          ...state,
+          {
+            ...product,
+            qty: 1,
+          },
+        ];
+      }
+      break;
+
+    case "DELITEM":
+      if (exist.qty === 1) {
+        return state.filter((x) => x.id !== exist.id);
+      } else {
+        return state.map((x) =>
+          x.id === product.id ? { ...x, qty: x.qty - 1 } : x
+        );
+      }
+      break;
+
+    default:
+      return state;
+      break;
+  }
+};
+
+export default handleCart;
