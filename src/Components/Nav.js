ae29708be7f3798ec773becdd1c69812e13d8f3e
Lucas Bendix Jolibois <lucasjolibois@icloud.com>
2023-03-11 18:26:24 +0100

    Update Nav.js

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -4,7 +4,9 @@ import { Link, Outlet } from 'react-router-dom'
 function Nav() {
   return (
     <>
+    <div className="mx-auto max-w-2xl mt-10 sm:px-6 lg:max-w-7xl lg:px-8">
     <Link to="/cart" className='underline text-red-800 italic font-normal'>cart</Link>
+    </div>
     <Outlet/>
     </>
   )
