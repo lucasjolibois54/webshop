5e31cb83b379182fd11f8b16fdc7e323a86c5c72
Lucas Bendix Jolibois <lucasjolibois@icloud.com>
2023-03-11 19:41:04 +0100

    Update Nav.js

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -5,8 +5,8 @@ function Nav() {
   return (
     <>
     <div className="mx-auto max-w-2xl mt-10 sm:px-6 lg:max-w-7xl lg:px-8">
-      <h2 className='mt-10 font-bold text-2xl'>FoodParadise</h2>
-    <Link to="/cart" className='-mt-4 underline text-red-800 italic font-normal float-right'>cart</Link>
+      <Link to="/" className='mt-10 font-bold text-2xl'>FoodParadise</Link>
+    <Link to="/cart" className='mt-1 underline text-red-800 italic font-normal float-right'>cart</Link>
     </div>
     <Outlet/>
     </>
